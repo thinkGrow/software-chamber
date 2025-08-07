d9bc67ec8542172d1223bf00e6f742501a7f5ea6
MaishaTahsin <rubaitreshad@gmail.com>
2025-08-08 04:11:07 +0600

    hero section : style : contact us btn

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,72 +4,97 @@ import Nav from "./Nav";
 
 const Hero = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-bl from-[#0f1d29] to-[#0b0f1a] text-white relative overflow-hidden">
+    // dotted bg
+    <div className="min-h-screen relative inset-0 bg-black pb-20 ">
+      {/* Black background */}
       <div
-        className="absolute inset-0 z-[1] object-cover bg-no-repeat brightness-125"
+        className="absolute inset-0 object-cover bg-no-repeat brightness-125"
         style={{
           backgroundImage: "url('vector/star.png')",
           backgroundPosition: "0px 90px",
         }}
       ></div>
-
-      {/* Vectors */}
-      {/* Top Left*/}
-      <div className="absolute -top-80 left-0">
-        <img src="vector/vector.png" alt="" />
-      </div>
-      {/* Bottom Left  */}
-      <div className="absolute top-0 -left-10">
-        <img src="vector/vector.png" alt="" />
-      </div>
-      {/* Top Right */}
-      <div className="absolute -top-30 right-0">
-        <img src="vector/vector2.png" alt="" />
-      </div>
-      {/* Bottom Left */}
-      <div className="absolute top-50 -right-40">
-        <img src="vector/vector2.png" alt="" />
-      </div>
-
-      <Nav></Nav>
-      <div className="text-center mt-20 space-y-2">
-        <div className="inline-block bg-teal-800/20 text-teal-400 text-sm px-4 py-1 rounded-full">
-          ★ Smarter Idea, Instant Solutions
+      <div className="text-white z-100">
+        {/* Vectors */}
+        {/* Top Left*/}
+        <div className="absolute -top-80 left-0">
+          <img src="vector/vector.png" alt="" />
         </div>
-        <h1 className="text-center text-4xl md:text-8xl font-bold leading-tight space-y-2 font-bri">
-          <span className="block text-[#00FFE0]  bg-clip-text">
-            We are your Software
-          </span>
+        {/* Bottom Left  */}
+        <div className="absolute top-0 -left-10">
+          <img src="vector/vector.png" alt="" />
+        </div>
+        {/* Top Right */}
+        <div className="absolute -top-30 right-0">
+          <img src="vector/vector2.png" alt="" />
+        </div>
+        {/* Bottom Left */}
+        <div className="absolute top-50 -right-40">
+          <img src="vector/vector2.png" alt="" />
+        </div>
 
-          <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-[#00FFE0] to-white">
-            Development
-            <span className="inline-block p-3 rounded-xl bg-[#00FFE0]/20 mx-2 items-center">
-              <img
-                src="software-license.png" // replace with actual path
-                alt="software icon"
-                className="w-8 h-8 object-contain"
-              />
-            </span>
-            <span className="inline-block">
-              <img
-                src="y.png" // replace with actual path
-                alt="software icon"
-                className="w-8 h-8 object-contain"
-              />
-            </span>
-            Team
-          </h1>
-          <span className="block text-white">in The Cloud</span>
+        <Nav></Nav>
+        <div className="text-center mt-20 space-y-2 relative">
+          <div className="relative inline-block p-4">
+            {/* dots */}
+            <svg className="absolute inset-0 w-full h-full pointer-events-none opacity-50">
+              <defs>
+                <pattern
+                  id="dots"
+                  width="24"
+                  height="24"
+                  patternUnits="userSpaceOnUse"
+                >
+                  <circle cx="1" cy="1" r="1" fill="rgba(255,255,255,.18)" />
+                </pattern>
+              </defs>
+              <rect width="100%" height="100%" fill="url(#dots)" />
+            </svg>
+            <div className="inline-block bg-teal-800/20 text-teal-400 text-sm px-4 py-1 rounded-full">
+              ★ Smarter Idea, Instant Solutions
+            </div>
+            <h1 className="text-center text-4xl md:text-8xl font-bold leading-tight space-y-2 font-bri">
+              <span className="block text-[#00FFE0]  bg-clip-text">
+                We are your Software
+              </span>
 
-          <div className="mt-6">
-            <button className="btn btn-neutral btn-md rounded-full px-6">
-              Contact Us
-            </button>
+              <h1 className="bg-clip-text text-transparent bg-gradient-to-b from-[#00FFE0] to-white">
+                Development
+                <span className="inline-block">
+                  <img
+                    src="hero/text-img.png"
+                    alt="software icon"
+                    className="w-[0.75em] h-[0.75em] object-fill ml-2 mr-1"
+                  />
+                </span>
+                <span className="inline-block">
+                  <img
+                    src="hero/text-img2.png"
+                    alt="software icon"
+                    className="w-[0.75em] h-[0.75em] object-fill ml-1 mr-2"
+                  />
+                </span>
+                Team
+              </h1>
+              <span className="block text-white">in The Cloud</span>
+
+              <div className="mt-6 flex justify-center z-10 font-outfit">
+                <button
+                  className="bg-gradient-to-bl text-2xl from-white/50 to-white/10 rounded-full px-4 py-2 flex items-center justify-center gap-4 backdrop-blur-3xl font-medium text-white 
+ "
+                >
+                  <p className="outline-title">Contact Us</p>
+                  <div className="bg-white rounded-full p-2">
+                    <img className="w-4 h-4" src="hero/arrow.png" alt="arrow" />
+                  </div>
+                </button>
+              </div>
+            </h1>
           </div>
-        </h1>
 
-        <div className="max-w-[90%] mx-auto mb-10">
-          <img src="hero_dummy.png" alt="" />
+          <div className="max-w-[90%] mx-auto mb-10">
+            <img src="hero_dummy.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
