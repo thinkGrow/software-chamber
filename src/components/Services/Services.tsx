0b80862ebaaab2c71b5fdd2c5aff396213e6feb7
MaishaTahsin <rubaitreshad@gmail.com>
2025-08-08 08:36:37 +0600

    service section : style : done (md)

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -17,57 +17,53 @@ const Services = () => {
     },
   ];
   return (
-    <section className="py-20 max-w-7xl mx-auto">
+    <section className="py-20 w-11/12 mx-auto">
       {/* Section Header */}
       <div className="flex justify-between items-center mb-12">
-        <h2 className="text-7xl font-black text-black flex items-center gap-2">
+        {/* Left Column */}
+        <h2 className="text-7xl text-black flex items-center gap-2 font-bri font-medium">
           Services
-          <img src="stars.png" alt="spark" className="w-6 h-6" />
+          <img
+            src="stars.png"
+            alt="spark"
+            className="w-10 h-10 relative right-5 top-0.5"
+          />
         </h2>
-        <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full shadow hover:bg-gray-100 transition">
-          <span className="text-sm font-medium">All Services</span>
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 5l7 7-7 7"
-            />
-          </svg>
+        {/* Right Column */}
+        <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full hover:bg-gray-100 transition font-outfit">
+          <span className="text-2xl font-medium tracking-tighter">
+            All Services
+          </span>
+
+
+          <div className="bg-gray-200 rounded-full w-10 h-10 flex items-center justify-center cursor-pointer hover:scale-105 transition">
+            <img className="h-5 w-5" src="next.png" alt="" />
+          </div>
         </button>
       </div>
 
       {/* Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:h-80">
         {services.map((item, idx) => (
           <div
             key={idx}
-            className={`flex flex-col justify-between rounded-[2rem] p-6 transition duration-300 ${
-              item.active ? "bg-[#25BEB4] text-white" : "bg-gray-100 text-black"
+            className={`flex flex-col justify-between rounded-[2rem] p-6 transition duration-300 hover:bg-[#25BEB4] hover:text-white bg-gray-100 text-black"
             }`}
           >
             <div>
-              <h3 className="text-2xl font-bold mb-4">{item.title}</h3>
+              <h3 className="text-5xl font-bri font-semibold mb-4">
+                {item.title}
+              </h3>
               <p
-                className={`text-sm ${
-                  item.active ? "text-white/80" : "text-gray-500"
-                }`}
+                className={`text-2xl font-outfit ${"hover:text-white/80 text-gray-500"}`}
               >
                 {item.desc}
               </p>
             </div>
             <div className="mt-8">
-              <button
-                className={`w-12 h-12 rounded-full bg-white shadow-md flex items-center justify-center hover:scale-105 transition`}
-              >
+              <button className="w-12 h-12 rounded-full bg-white shadow-md flex items-center justify-center hover:scale-105 transition cursor-pointer hover:text-[#25BEB4] text-black">
                 <svg
-                  className={`w-4 h-4 ${
-                    item.active ? "text-[#25BEB4]" : "text-black"
+                  className={`w-4 h-4"
                   }`}
                   fill="none"
                   stroke="currentColor"
