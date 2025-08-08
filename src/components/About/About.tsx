b8dc5b16a97704b475c1a3c18862862efbff97c6
MaishaTahsin <rubaitreshad@gmail.com>
2025-08-08 07:58:14 +0600

    about section : style : done (md)

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,9 +8,9 @@ const About = () => {
             <img src="stars.png" alt="stars" />
           </span>
           At Software Chamber,
-          <span className="text-gray-400"> we specialize</span>
+          <span className="text-neutral-400"> we specialize</span>
           in turning complex challenges into elegant
-          <span className="text-gray-400"> digital solutions</span>
+          <span className="text-neutral-400"> digital solutions</span>
           <span className="inline-block">
             <img src="about/stock.png" alt="stock" />
           </span>
@@ -22,21 +22,21 @@ const About = () => {
         {/* Left Text */}
         <div className="min-h-full">
           <div className="h-full flex flex-col justify-between text-left space-y-8 py-10">
-            <p className="text-4xl font-semibold">
+            <p className="text-4xl font-bri font-medium">
               20+
-              <span className="block text-2xl font-light text-gray-300">
+              <span className="block text-2xl font-light text-gray-400 font-outfit">
                 Tech Partners
               </span>
             </p>
-            <p className="text-4xl font-semibold">
+            <p className="text-4xl font-bri font-medium">
               150+
-              <span className="block text-2xl font-light text-gray-300">
+              <span className="block text-2xl font-light text-gray-400 font-outfit">
                 Project Completed
               </span>
             </p>
-            <p className="text-4xl font-semibold">
+            <p className="text-4xl font-bri font-medium">
               1K+
-              <span className="block text-2xl font-light text-gray-300">
+              <span className="block text-2xl font-light text-gray-400 font-outfit">
                 Satisfied Customers
               </span>
             </p>
@@ -73,12 +73,21 @@ const About = () => {
             },
           ].map((item, i) => (
             <div key={i} className="flex gap-4 items-start">
-              <div className="w-8 h-8 rounded-full p-3 bg-[#00FFE0] text-black font-bold flex items-center justify-center">
-                {i + 1}
+              {/* Outer circle */}
+              <div className="size-10 aspect-square shrink-0 rounded-full bg-[#27B4A3] flex items-center justify-center">
+                {/* Inner circle */}
+                <div className="size-6 aspect-square shrink-0 rounded-full bg-white flex items-center justify-center text-[#436255] text-xs font-semibold leading-none">
+                  {i + 1}
+                </div>
               </div>
+
               <div>
-                <h4 className="font-semibold text-black">{item.title}</h4>
-                <p className="text-sm text-gray-300 mt-1">{item.desc}</p>
+                <h4 className="font-semibold text-black font-bri">
+                  {item.title}
+                </h4>
+                <p className="text-sm font-outfit text-gray-400 mt-1">
+                  {item.desc}
+                </p>
               </div>
             </div>
           ))}
