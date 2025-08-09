9961ae65a87a0d2e36c3f3c4ce2d58acbf16bc77
MaishaTahsin <rubaitreshad@gmail.com>
2025-08-10 04:53:29 +0600

    faq section : feat : accordion functional style : customized borders and color change when selected

--- src/components/faq/Accordion.tsx
+++ src/components/faq/Accordion.tsx
@@ -1,46 +1,67 @@
-// import React from 'react';
+import { useState } from "react";
 
 const Accordion = () => {
+  const [openIndex, setOpenIndex] = useState(0);
+
+  const items = [
+    {
+      q: "What software development services does your company offer?",
+      a: 'Click the "Sign Up" button in the top right corner and follow the registration process.',
+    },
+    {
+      q: "What industries do you specialize in?",
+      a: 'Click on "Forgot Password" on the login page and follow the instructions sent to your email.',
+    },
+    {
+      q: "What sets your company apart from the competition?",
+      a: 'Go to "My Account" settings and select "Edit Profile" to make changes.',
+    },
+    {
+      q: "Is my project idea and information kept confidential?",
+      a: 'Go to "My Account" settings and select "Edit Profile" to make changes.',
+    },
+  ];
+
   return (
     <div className="join join-vertical bg-base-100 w-full text-left">
-      <div className="collapse collapse-arrow join-item border-base-300 border">
-        <input type="radio" name="my-accordion-4" defaultChecked />
-        <div className="collapse-title font-semibold">
-          What software development services does your company offer?
-        </div>
-        <div className="collapse-content text-sm">
-          Click the "Sign Up" button in the top right corner and follow the
-          registration process.
-        </div>
-      </div>
-      <div className="collapse collapse-arrow join-item border-base-300 border">
-        <input type="radio" name="my-accordion-4" />
-        <div className="collapse-title font-semibold">
-          What industries do you specialize in?
-        </div>
-        <div className="collapse-content text-sm">
-          Click on "Forgot Password" on the login page and follow the
-          instructions sent to your email.
-        </div>
-      </div>
-      <div className="collapse collapse-arrow join-item border-base-300 border">
-        <input type="radio" name="my-accordion-4" />
-        <div className="collapse-title font-semibold">
-          What sets your company apart from the competition?
-        </div>
-        <div className="collapse-content text-sm">
-          Go to "My Account" settings and select "Edit Profile" to make changes.
-        </div>
-      </div>
-      <div className="collapse collapse-arrow join-item border-base-300 border">
-        <input type="radio" name="my-accordion-4" />
-        <div className="collapse-title font-semibold">
-          Is my project idea and information kept confidential?
-        </div>
-        <div className="collapse-content text-sm">
-          Go to "My Account" settings and select "Edit Profile" to make changes.
-        </div>
-      </div>
+      {items.map((item, i) => {
+        const isOpen = openIndex === i;
+
+        return (
+          <div
+            key={i}
+            className="collapse join-item border-base-300 border-b py-5"
+          >
+            <input
+              type="radio"
+              name="my-accordion"
+              checked={isOpen}
+              onChange={() => setOpenIndex(i)}
+            />
+            <div className="collapse-title font-semibold flex justify-between items-center cursor-pointer">
+              {item.q}
+
+              {/* Different button for open/closed */}
+              {isOpen ? (
+                // open
+                <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full hover:bg-gray-100 transition font-outfit">
+                  <div className="bg-[#2CCEBA] rounded-full w-10 h-10 flex items-center justify-center cursor-pointer hover:scale-105 transition ml-2">
+                    <img className="h-5 w-5" src="up.png" alt="" />
+                  </div>
+                </button>
+              ) : (
+                // closed
+                <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full hover:bg-gray-100 transition font-outfit">
+                  <div className="bg-black rounded-full w-10 h-10 flex items-center justify-center cursor-pointer hover:scale-105 transition ml-2">
+                    <img className="h-5 w-5" src="down.png" alt="" />
+                  </div>
+                </button>
+              )}
+            </div>
+            <div className="collapse-content text-sm">{item.a}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -2,10 +2,10 @@ import Accordion from "./Accordion";
 
 const FAQ = () => {
   return (
-    <section className="relative py-24 px-4 text-center w-11/12 mx-auto">
+    <section className="relative py-24 px-4 text-center w-11/12 mx-auto mb-10">
       {/* FAQ Title */}
       <div>
-        <h1 className="text-7xl font-bri text-center text-balance">
+        <h1 className="text-7xl font-bri text-center text-balance font-medium pb-10">
           Comprehensive Answers to the <br />
           <span className="inline-block">
             <img src="stars.png" alt="stars" />
@@ -20,25 +20,25 @@ const FAQ = () => {
       </div>
 
       {/* Section Header */}
-      <div className="flex justify-between items-center my-12 ">
-        <h2 className="text-7xl font-bri text-black flex items-center gap-2">
-          FAQ
-        </h2>
-        <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full shadow hover:bg-gray-100 transition">
-          <span className="text-sm font-medium">All FAQ</span>
-          <svg
-            className="w-4 h-4"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth={2}
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 5l7 7-7 7"
-            />
-          </svg>
+      <div
+        className="flex justify-between items-center my-12 border-b-4 border-transparent pb-10"
+        style={{
+          borderImage:
+            "repeating-linear-gradient(to right, gray 0, gray 20px, transparent 20px, transparent 40px) 30",
+        }}
+      >
+        <div>
+          <h2 className="text-7xl font-bri text-black flex items-center gap-2 font-medium">
+            FAQ
+          </h2>
+        </div>
+
+        <button className="flex items-center gap-2 bg-white text-black px-4 py-2 rounded-full hover:bg-gray-100 transition font-outfit">
+          <span className="text-2xl font-medium tracking-tighter">All FAQ</span>
+
+          <div className="bg-[#2CCEBA] rounded-full w-10 h-10 flex items-center justify-center cursor-pointer hover:scale-105 transition ml-2">
+            <img className="h-5 w-5" src="next.png" alt="" />
+          </div>
         </button>
       </div>
       {/* Accordion */}
